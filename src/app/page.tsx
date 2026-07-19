869484ed9a24bd7628f1f481ab5d4a95788941a3
Anushka Shirke <anushkashirke.trinit@gmail.com>
2026-07-19 08:38:46 +0000

    match the color of text "" We design clean and user-focused interfaces t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -352,7 +352,7 @@ export default function Home() {
               {/* Added Description */}
               <motion.p
                 variants={overlayVariants}
-                className="font-playground italic lowercase text-accent text-xl md:text-3xl mt-8 max-w-2xl text-left leading-relaxed"
+                className="font-playground italic lowercase text-background text-xl md:text-3xl mt-8 max-w-2xl text-left leading-relaxed"
               >
                 We design clean and user-focused interfaces that enhance usability and engagement. By combining creativity with strategic thinking, we deliver experiences that are both functional and visually compelling.
               </motion.p>
