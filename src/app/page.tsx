63622733f7337703371b40a2e08ecc7862b75794
Anushka Shirke <anushkashirke.trinit@gmail.com>
2026-08-17 07:41:30 +0000

    can you change the text "lets create"  to "Let's create" dont make any o

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -261,8 +261,8 @@ export default function Home() {
           </motion.p>
           
           {/* Centered typing animation text with highlight */}
-          <h1 className="text-[clamp(48px,12vw,200px)] font-thunder text-primary lowercase tracking-tight leading-none">
-            lets create <br />
+          <h1 className="text-[clamp(48px,12vw,200px)] font-thunder text-primary tracking-tight leading-none">
+            Let's create <br />
             <span className="relative inline-block bg-[#C0C0C0] text-[#DC9632] px-4">
               {displayText}
               <motion.div 
